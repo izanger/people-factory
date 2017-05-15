58f195ca95d2c05c6b3db3eee1d9b13bde492ebd
Ian Zanger <izanger@purdue.edu>
2017-05-15 14:38:25 -0400

    Add looping function method of appending list

---
+++ README.md
@@ -0,0 +1,14 @@
+# Day 1
+
+## Homework
+
+Add form values to `.details` using `document.createElement` and `appendChild`, instead of `innerHTML`.
+
+### Bonus Credit
+
+Break out some of this functionality into a separate function.
+
+### Super Mega Bonus Credit
+
+Do not hard-code the names of the fields in your Javascript.
+Loop over all of the elements in the form.
--- index.js
+++ index.js
@@ -13,8 +13,12 @@ const handleSubmit = (ev) => {
   const colorDiv = `
     <div style="height: 50px; width: 100px; background-color: ${hairColor}"></div>
   `
-//   details.innerHTML = `<em>${personName}</em>`
+  //details.innerHTML = `<em>${personName}</em>`
 
+//external looping function method
+  addList(details, form)
+
+  //simple homework solution
 //   const em = document.createElement('em')
 //   em.textContent = personName
 //   details.appendChild(em)
@@ -30,14 +34,44 @@ const handleSubmit = (ev) => {
 //   li2.textContent = 'Hair Color: ' + form.hairColor.value
 //   ul.appendChild(li2)
 
-  details.innerHTML = `
-    <ul>
-        <li>Name: ${personName}</li>
-        <li>Hair Color: ${colorDiv}</li>
-        <li>Age: ${age}</li>
-        <li>Birthlace: ${birthPlace}</li>
-    </ul>
-  `
+//   const li3 = document.createElement('li')
+//   li3.textContent = 'Age: ' + form.age.value
+//   ul.appendChild(li3)
+
+//   const li4 = document.createElement('li')
+//   li4.textContent = 'Birthplace: ' + form.birthPlace.value
+//   ul.appendChild(li4)
+
+
+//easy visual way
+//   details.innerHTML = `
+//     <ul>
+//         <li>Name: ${personName}</li>
+//         <li>Hair Color: ${colorDiv}</li>
+//         <li>Age: ${age}</li>
+//         <li>Birthplace: ${birthPlace}</li>
+//     </ul>
+//   `
+
+
 }
 
-personForm.addEventListener('submit', handleSubmit)
+personForm.addEventListener('submit', handleSubmit) //addEventListener can be used multiple times on the same thing
+
+function addList(addBeneath, form) {
+    //var elements = form.elements;
+    var elements = form.getElementsByTagName('input')
+    var labels = form.getElementsByTagName('label')
+    
+    const ul = document.createElement('ul')
+    addBeneath.appendChild(ul)
+    for(var i = 0; i < elements.length; i++){
+        if(labels[i].nodeName === 'LABEL'){
+            const li = document.createElement('li')
+            li.textContent = labels[i].textContent + ': ' + elements[i].value
+            ul.appendChild(li)
+        }
+
+    }
+
+}
